c5e595e0331659beb327572c92f35aa1c396a20d
20208237T <ezsuarezs@axpecantabria.com>
2022-06-21 11:52:02 +0200

    new file:   pruebas.java

---
+++ pruebas.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class pruebasdemierda {
+	
+	static Utils utils = new Utils();
+	
+	public static void main(String[] args) {
+
+	System.out.println(utils.binaryToDecimal(1000));
+	
+	String test = "xxaxx";
+	
+	System.out.println((char) 97);
+	
+	}
+			
+	
+}
