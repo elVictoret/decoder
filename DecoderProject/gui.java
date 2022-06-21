6fdde57342b0f47609c025226a09fff8223f9544
20208237T <ezsuarezs@axpecantabria.com>
2022-06-21 21:02:59 +0200

    modified:   DecoderProject/Decoder.java 	modified:   DecoderProject/Main.java 	modified:   DecoderProject/gui.java 	modified:   DecoderProject/pruebas.java

--- DecoderProject/gui.java
+++ DecoderProject/gui.java
@@ -1,42 +1,69 @@
 import javax.swing.*;
-import java.awt.*;
-class gui {
-    public static void main(String args[]) {
 
-        //Creating the Frame
-        JFrame frame = new JFrame("Chat Frame");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-        //Creating the MenuBar and adding components
-        JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("FILE");
-        JMenu m2 = new JMenu("Help");
-        mb.add(m1);
-        mb.add(m2);
-        JMenuItem m11 = new JMenuItem("Open");
-        JMenuItem m12 = new JMenuItem("Save as");
-        m1.add(m11);
-        m1.add(m12);
+public class gui {
+		
+	public void environment() {  
+    JFrame f=new JFrame("Decoder by Victoret");  
+    
+    final JTextField tf=new JTextField();  
+    tf.setBounds(50,50,300,20);  
+    
+    final JTextField tf2=new JTextField();  
+    tf2.setBounds(50,250,300,20);  
+    
+    JLabel label1 = new JLabel("Encoded text");
+    label1.setBounds(50,30,300,20);
+    JLabel label2 = new JLabel("Decoded text");
+    label2.setBounds(50,230,300,20);
+    
+    JButton button1=new JButton("base32");
+    JButton button2=new JButton("base64");
+    JButton button3=new JButton("rot13");
+    JButton button4=new JButton("Caesar cipher");
+    
+    button1.setBounds(50,100,100,30);  
+    button1.addActionListener(new ActionListener(){  
+	public void actionPerformed(ActionEvent e){  
+	            tf.setText("base32");  
+				}  
+	    	});  
+	    
+    button2.setBounds(50,150,100,30);  
+    button2.addActionListener(new ActionListener(){  
+	public void actionPerformed(ActionEvent e){  
+	            tf.setText("base64");  
+				}  
+	    	});  
+	    
+    button3.setBounds(250,100,100,30);  
+    button3.addActionListener(new ActionListener(){  
+	public void actionPerformed(ActionEvent e){  
+	            tf.setText("rot13");  
+				}  
+	    	});  
+	 
+    button4.setBounds(250,150,100,30);  
+    button4.addActionListener(new ActionListener(){  
+	public void actionPerformed(ActionEvent e){  
+	            tf.setText("Caesar cipher");
+				} 
+	    	});  
+    
+    
+    f.add(button1);
+    f.add(button2);
+    f.add(button3);
+    f.add(button4);
+    f.add(tf);
+    f.add(tf2);
+    f.add(label1);
+    f.add(label2);
+    f.setSize(400,400);  
+    f.setLayout(null);  
+    f.setVisible(true);   
 
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
-        JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JButton send = new JButton("Send");
-        JButton reset = new JButton("Reset");
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
-        panel.add(send);
-        panel.add(reset);
-
-        // Text Area at the Center
-        JTextArea ta = new JTextArea();
-
-        //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.getContentPane().add(BorderLayout.NORTH, mb);
-        frame.getContentPane().add(BorderLayout.CENTER, ta);
-        frame.setVisible(true);
-    }
+	}
 }
